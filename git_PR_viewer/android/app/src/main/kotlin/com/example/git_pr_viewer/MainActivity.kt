dea997d9273f5a618edf7c780aa2e9e8f8ccdba2
Ap00rvB <143342361+Ap00rvB@users.noreply.github.com>
2025-08-31 20:11:55 +0530

    Delete git_PR_viewer directory

--- git_PR_viewer/android/app/src/main/kotlin/com/example/git_pr_viewer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.git_pr_viewer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- git_PR_viewer/lib/utils/date_formatter.dart
+++
@@ -1,8 +0,0 @@
-// lib/core/utils/date_formatter.dart
-import 'package:intl/intl.dart';
-
-class DateFormatter {
-  static String formatDateTime(DateTime dateTime) {
-    return DateFormat('MMM dd, yyyy HH:mm').format(dateTime);
-  }
-}
