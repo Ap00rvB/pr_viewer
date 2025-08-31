4358de1d413e297a2d5cc34d699072b04be9e4de
Apoorv <apoorv.bhardwaj@3five8.com>
2025-08-31 19:26:18 +0530

    Initial commit

---
+++ pr_viewer/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:pr_viewer/screens/pr_list_screen/pr_list_screen.dart';
+
+import 'package:pr_viewer/services/routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return (const MaterialApp(
+      onGenerateRoute: routeGenerator,
+      initialRoute: PrListScreen.routeName,
+      debugShowCheckedModeBanner: false,
+      title: "PR List Screen",
+    ));
+  }
+}
